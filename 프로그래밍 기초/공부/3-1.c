8653c40d6028c64f15b8e05c6258c1645b6bfbca
illulia <62697685+illulia@users.noreply.github.com>
2020-10-05 10:28:24 +0900

    .

--- 프로그래밍 기초/공부/3-1.c
+++ 프로그래밍 기초/공부/3-1.c
@@ -3,18 +3,9 @@
 int main()
 {
 	int a = 2147483647;
-	float i = 1.175494e-38;
-	float j = 3.402823e+38;
 	
 	printf("정수 최대값:%d\n", a);
 	printf("오버플로우 후 값:%d\n", a+1);
-	
-	printf("실수 최대값:%e\n", a);
-	printf("오버플로우 후 값:%f\n", a+1);
-	
-	
-	printf("실수 최소값:%f\n", a);
-	printf("언플로우 후 값:%f\n", a+1);
-	
+
 	return 0;
 }
---
+++ 프로그래밍 기초/공부/3-5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int year;
+	long spy = 31560000;
+	
+	scanf("%d", &year);
+	
+	printf("%ld", year*spy);
+	return 0;
+}
